e607a2272e7097f589d42b6648a3cdd24bf40eb9
sadowrock <nguyenhien.nohats@gmail.com>
2019-08-26 23:01:32 +0700

    update

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const mongoose = require("mongoose");
 const User = require("./model/users");
 const bodyParser = require("body-parser");
 const path = require("path");
+const dotenv = require("dotenv").config();
+
 
 //connect db
-mongoose.connect("mongodb://localhost:27017/blogapi", err => {
+mongoose.connect(process.env.DB_HOST, err => {
   if (err) throw err;
   console.log("Successfully connected");
 });
@@ -79,14 +81,13 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
